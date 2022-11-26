65f4ff5d85c366a439994b795d8902fe08da1885
Suraj kumar <90993575+Surajmicky@users.noreply.github.com>
2022-11-26 13:20:13 +0530

    added components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,166 +1,163 @@
-import React from 'react'
-import styled from 'styled-components';
-import {Link, useNavigate} from 'react-router-dom';
-import Logo from '../icons/whatsapp.png'
-import {useState,useEffect} from 'react';
-import {ToastContainer,toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios';
-import {loginRoute} from '../utils/APIRoutes';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { loginRoute } from "../utils/APIRoutes";
 
-const Login = () => {
-    const [values,setValues]=useState({
-        username:"",
-        password:"",
-        confirmPassword:""
-    });
-    
-    useEffect(()=>{
-      if(localStorage.getItem('chat-app-usr')){
-        navigate('/chat')
+export default function Login() {
+  const navigate = useNavigate();
+  const [values, setValues] = useState({ username: "", password: "" });
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const validateForm = () => {
+    const { username, password } = values;
+    if (username === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    } else if (password === "") {
+      toast.error("Email and Password is required.", toastOptions);
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      const { username, password } = values;
+      const { data } = await axios.post(loginRoute, {
+        username,
+        password,
+      });
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
       }
-    },[])
-    const navigate= useNavigate()
-    const handleChange=(event) => {
-      setValues({...values,[event.target.name]:event.target.value})
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
 
-    }
-    const toastCSS={
-      position:'bottom-right',
-      autoClose:6000,
-      pauseOnHover:true,
-      draggable:true,
-      theme:'dark'
-
-    }
-    const handleSubmit= async(event)=>{
-       event.preventDefault();
-   
-       if(handleValidation()){
-            const {username,password}=values;
-             console.log(username,password)
-            let {data}=await axios.post(loginRoute,{username,password});
-       
-            if(data.status===false){
-              toast.error(data.message,toastCSS)
-            }
-            if(data.status===true ){
-              localStorage.setItem('chat-app-usr',JSON.stringify(data.token));
-              // setCookies('token',data.token,'/')
-             navigate('/chat')
-            }
-       }
-     
-       
-
-    }
- 
-    const handleValidation=()=>{
-       let {username,password}=values;
-      if(username.length<3 || password.length===''){
-        toast.error('please enter registered username and password',toastCSS);
-        return false;
+        navigate("/");
       }
-      return true;
     }
+  };
+
   return (
     <>
-    <FormContainer>
-   
-    <form onSubmit={(event)=>handleSubmit(event)}>
-    <div className="form_div">
-    <img src={Logo} alt="" />
-    <h1>Chatty</h1>
-    </div>
-        <input type="name" name="username" placeholder="Name" onChange={(event)=>handleChange(event)} />
-        <input type="password" name="password" placeholder="Password" onChange={(event)=>handleChange(event)} />
-        <button>Submit</button>
-        <ToastContainer/>
-        <span>Don't have an Account ? <Link to="/register">Sign up</Link></span>
-      
-    </form>
-    
-    </FormContainer>
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+            min="3"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Log In</button>
+          <span>
+            Don't have an account ? <Link to="/register">Create One.</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
     </>
-  )
+  );
 }
-const FormContainer= styled.div`
-  height:100vh;
-  width:100vw;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  gap:1rem;
-  align-items:center;
-  background-color:#131324;
-  .form_div{
-    display:flex;
-    gap:1rem;
-    align-items:center;
-    justify-content:center;
-   
-    img{
-    height:5rem;
-  }
-   
-  h1{
-    color:white;
-    text-transform:uppercase;
-  }
 
-  }
- 
-  
-  form{
-    display:flex;
-    flex-direction:column;
-    gap:1.5rem;
-    align-items:center;
-    background-color:#00000076;
-    padding:2.5rem 3.5rem;
-    border-radius:2rem;
-  }
- 
-  input{
-    background-color:transparent;
-    padding:1rem;
-    border-radius:0.4rem;
-    border:0.1rem solid #4e0eff;
-    font-size:1rem;
-    color:white;
-    width:100%;
-    &:focus{
-        border:0.1rem solid #997af0;
-        outline:none;
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
     }
   }
-  button{
-    background-color:#997af0;
-    color:white;
-    padding:1rem 2rem;
-    border:none;
-    border-radius:0.4rem;
-    font-weight:bold;
-    cursor:pointer;
-    font-size:1rem;
-    text-transform:uppercase;
-    transition:0.5s ease-in-out;
-    &:hover{
-        background-color:#4e0eff;
 
-    }
-    
-
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 5rem;
   }
-  span{
-        color:white;
-        text-transform:uppercase;
-        a{
-            color:#4e0eff;
-            text-decoration:none;
-            font-weight:bold;
-        }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
     }
-
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
 `;
-export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,178 +1,197 @@
-import React from 'react'
-import styled from 'styled-components';
-import {Link, useNavigate} from 'react-router-dom';
-import Logo from '../icons/whatsapp.png'
-import {useState,useEffect} from 'react';
-import {ToastContainer,toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { registerRoute } from "../utils/APIRoutes";
 
-import axios from 'axios';
-import {registerRoute} from '../utils/APIRoutes';
+export default function Register() {
+  const navigate = useNavigate();
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-const Register = () => {
-    const [values,setValues]=useState({
-        username:"",
-        email:"",
-        password:"",
-        confirmPassword:""
-    })
-  useEffect(()=>{
-    if(localStorage.getItem('chat-app-usr')){
-      navigate('/chat')
+  useEffect(() => {
+    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+      navigate("/");
     }
-  },[])
-    const navigate= useNavigate()
-    const handleChange=(event) => {
-      setValues({...values,[event.target.name]:event.target.value})
+  }, []);
 
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error(
+        "Password and confirm password should be same.",
+        toastOptions
+      );
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters.",
+        toastOptions
+      );
+      return false;
+    } else if (email === "") {
+      toast.error("Email is required.", toastOptions);
+      return false;
     }
-    const toastCSS={
-      position:'bottom-right',
-      autoClose:6000,
-      pauseOnHover:true,
-      draggable:true,
-      theme:'dark'
 
-    }
-    const handleSubmit= async(event)=>{
-       event.preventDefault();
-       
-       if(handleValidation()){
-            const {username,email,password}=values;
-             console.log(username,email,password)
-            let {data}=await axios.post(registerRoute,{username,email,password});
-       
-            if(data.status===false){
-              toast.error(data.message,toastCSS)
-            }
-            if(data.status===true){
-              localStorage.setItem('chat-app-usr',JSON.stringify(data));
+    return true;
+  };
 
-             navigate('/login')
-            }
-       }
-     
-       
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      const { email, username, password } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
 
-    }
- 
-    const handleValidation=()=>{
-       let {username,email,password,confirmPassword}=values;
-      if(username.length<3){
-        toast.error('Username should be at least 3 characters',toastCSS);
-        return false;
-      }else if(email===''){
-        toast.error('please enter valid email',toastCSS);
-        return false;
-      }else if(password.length<8){
-        toast.error('password should be at least 8 characters',toastCSS);
-        return false;
-      }else if(password!==confirmPassword){
-        toast.error('password and confirm password do not match',toastCSS);
-        return false;
+      if (data.status === false) {
+        toast.error(data.msg, toastOptions);
       }
-      return true;
+      if (data.status === true) {
+        localStorage.setItem(
+          process.env.REACT_APP_LOCALHOST_KEY,
+          JSON.stringify(data.user)
+        );
+        navigate("/");
+      }
     }
+  };
+
   return (
     <>
-    <FormContainer>
-   
-    <form onSubmit={(event)=>handleSubmit(event)}>
-    <div className="form_div">
-    <img src={Logo} alt="" />
-    <h1>Chatty</h1>
-    </div>
-        <input type="name" name="username" placeholder="Name" onChange={(event)=>handleChange(event)} />
-        <input type="email" name="email" placeholder="Email" onChange={(event)=>handleChange(event)} />
-        <input type="password" name="password" placeholder="Password" onChange={(event)=>handleChange(event)} />
-        <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={(event)=>handleChange(event)} />
-        <button>Submit</button>
-        <ToastContainer/>
-        <span>Already have an account ? <Link to="/login">Login</Link></span>
-      
-    </form>
-    
-    </FormContainer>
+      <FormContainer>
+        <form action="" onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            <img src={Logo} alt="logo" />
+            <h1>snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login.</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer />
     </>
-  )
+  );
 }
-const FormContainer= styled.div`
-  height:100vh;
-  width:100vw;
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  gap:1rem;
-  align-items:center;
-  background-color:#131324;
-  .form_div{
-    display:flex;
-    gap:1rem;
-    align-items:center;
-    justify-content:center;
-   
-    img{
-    height:5rem;
-  }
-   
-  h1{
-    color:white;
-    text-transform:uppercase;
-  }
 
-  }
- 
-  
-  form{
-    display:flex;
-    flex-direction:column;
-    gap:1.5rem;
-    align-items:center;
-    background-color:#00000076;
-    padding:2.5rem 3.5rem;
-    border-radius:2rem;
-  }
- 
-  input{
-    background-color:transparent;
-    padding:1rem;
-    border-radius:0.4rem;
-    border:0.1rem solid #4e0eff;
-    font-size:1rem;
-    color:white;
-    width:100%;
-    &:focus{
-        border:0.1rem solid #997af0;
-        outline:none;
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
     }
   }
-  button{
-    background-color:#997af0;
-    color:white;
-    padding:1rem 2rem;
-    border:none;
-    border-radius:0.4rem;
-    font-weight:bold;
-    cursor:pointer;
-    font-size:1rem;
-    text-transform:uppercase;
-    transition:0.5s ease-in-out;
-    &:hover{
-        background-color:#4e0eff;
 
-    }
-    
-
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
   }
-  span{
-        color:white;
-        text-transform:uppercase;
-        a{
-            color:#4e0eff;
-            text-decoration:none;
-            font-weight:bold;
-        }
+  input {
+    background-color: transparent;
+    padding: 1rem;
+    border: 0.1rem solid #4e0eff;
+    border-radius: 0.4rem;
+    color: white;
+    width: 100%;
+    font-size: 1rem;
+    &:focus {
+      border: 0.1rem solid #997af0;
+      outline: none;
     }
-
+  }
+  button {
+    background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
+  }
+  span {
+    color: white;
+    text-transform: uppercase;
+    a {
+      color: #4e0eff;
+      text-decoration: none;
+      font-weight: bold;
+    }
+  }
 `;
-export default Register
